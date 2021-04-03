5ab123d20798cabf98c88e97d02bcdb446769434
Max Rebedailo <akrosom21@gmail.com>
2021-04-04 01:39:52 +0300

    feat: add appearing spacemen according to score in sprint

--- src/helpers/hooks.ts
+++ src/helpers/hooks.ts
@@ -1,27 +1,45 @@
-import useSound from "use-sound";
-import { WordsType } from "../store/reducers/audioSlice";
+import useSound from 'use-sound';
+import { WordsType } from '../store/reducers/audioSlice';
+import cosmo_2 from '../assets/image/cosmo_2.png';
+import cosmo_3 from '../assets/image/cosmo_3.png';
+import cosmo_4 from '../assets/image/cosmo_4.png';
+import cosmo_1 from '../assets/image/cosmo_1.png';
 
 export const useRandom = (length: number) => {
-    const random = Math.random();
-    const randomIndex = Math.round(0 - 0.5 + Math.random() * (length - 1));
-    return { random, randomIndex }
-}
+  const random = Math.random();
+  const randomIndex = Math.round(0 - 0.5 + Math.random() * (length - 1));
+  return { random, randomIndex };
+};
 
 export const useAudio = (sound: string) => {
-    const [play] = useSound(sound)
-    return play
-}
+  const [play] = useSound(sound);
+  return play;
+};
 
 export const useShuffle = (array: Array<WordsType>, word: string) => {
-const shuffledArr = array.filter((w: WordsType)=> w.wordTranslate !== word).sort(()=>Math.random() - 0.5)
-return [shuffledArr[0].wordTranslate,
-shuffledArr[1].wordTranslate,
-shuffledArr[2].wordTranslate,
-word
-].sort(()=> Math.random() - 0.5)
-}
+  const shuffledArr = array.filter((w: WordsType) => w.wordTranslate !== word).sort(() => Math.random() - 0.5);
+  return [shuffledArr[0].wordTranslate, shuffledArr[1].wordTranslate, shuffledArr[2].wordTranslate, word].sort(
+    () => Math.random() - 0.5
+  );
+};
 
 export const useRandomPage = () => {
-    const MAX_PAGE = 29
-    return Math.floor(Math.random() * MAX_PAGE);
-}
+  const MAX_PAGE = 29;
+  return Math.floor(Math.random() * MAX_PAGE);
+};
+
+export const useIconsArray = (gameScore: number) => {
+  const array = [cosmo_1, cosmo_2, cosmo_3, cosmo_4];
+  if (gameScore >= 200) {
+    array.length = 4;
+  } else if (gameScore >= 100) {
+    array.length = 3;
+  } else if (gameScore >= 40) {
+    array.length = 2;
+  } else if (gameScore >= 20) {
+    array.length = 1;
+  } else if (gameScore < 20) {
+    array.length = 0;
+  }
+  return array;
+};
--- src/Сomponents/Games/Sprint/Spacemen/Spacemen.tsx
+++ src/Сomponents/Games/Sprint/Spacemen/Spacemen.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import cosmo_2 from '../../../../assets/image/cosmo_2.png';
-import cosmo_3 from '../../../../assets/image/cosmo_3.png';
-import cosmo_4 from '../../../../assets/image/cosmo_4.png';
-import cosmo_1 from '../../../../assets/image/cosmo_1.png';
-import styles from './Spacemen.module.scss'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import styles from './Spacemen.module.scss';
+import { score } from '../../../../store/reducers/sprintSlice';
+import { useIconsArray } from '../../../../helpers/hooks';
 
 export const Spacemen = () => {
-    const cosmoIcons = [cosmo_1, cosmo_2, cosmo_3, cosmo_4]
-    return (
-        <div className={styles.cosmo__icons}>
-            {cosmoIcons.map(icon => <img key={icon} src={icon} alt="cosmo-icon" className={styles.cosmo__icon} />)}
-        </div>
-    )
-}
+  const gameScore = useSelector(score);
+  const cosmoIcons = useIconsArray(gameScore);
+  return (
+    <div className={styles.cosmo__icons}>
+      {cosmoIcons.map((icon, index) => (
+        <img key={icon} src={icon} alt="cosmo-icon" className={styles.cosmo__icon} />
+      ))}
+    </div>
+  );
+};
