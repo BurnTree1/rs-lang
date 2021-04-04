f056eb21a83611e5de11c8cb41dac699a2546670
Ilya Yeliseyeu <Ilya.Yeliseyeu@ibm.com>
2021-04-04 14:38:03 +0300

    feat: add team board

--- src/Сomponents/LandingPage/Team/Team.tsx
+++ src/Сomponents/LandingPage/Team/Team.tsx
@@ -1,35 +1,25 @@
-import React from "react";
+import React, { FC } from "react";
+import { List, ListItem } from "@material-ui/core";
+import { Done } from "@material-ui/icons";
 import styles from "./Team.module.scss";
-import avatar from "../../../assets/image/avatar.jpg";
+import { TeamType, team } from "../../../helpers";
 
 export const Team = () => (
   <div className={styles.team}>
     <h2 className={styles.title}>Наша команда</h2>
     <div className={styles.team__inner}>
-      <div className={styles.team__item}>
-        <div className={styles.photo}>
-          <img src={avatar} alt="avatar" className={styles.photo__img}/>
-        </div>
-        <div className={styles.text}>Lorem ipsum</div>
-      </div>
-      <div className={styles.team__item}>
-        <div className={styles.photo}>
-          <img src={avatar} alt="avatar" className={styles.photo__img}/>
-        </div>
-        <div className={styles.text}>Lorem ipsum</div>
-      </div>
-      <div className={styles.team__item}>
-        <div className={styles.photo}>
-          <img src={avatar} alt="avatar" className={styles.photo__img}/>
-        </div>
-        <div className={styles.text}>Lorem ipsum</div>
-      </div>
-      <div className={styles.team__item}>
-        <div className={styles.photo}>
-          <img src={avatar} alt="avatar" className={styles.photo__img}/>
-        </div>
-        <div className={styles.text}>Lorem ipsum</div>
-      </div>
+      {team.map(member => <TeamItem key={member.role} {...member}/>)}
     </div>
   </div>
 );
+
+const TeamItem: FC<TeamType> = ({ link, role, image, dev }) => (
+  <div className={styles.team__item}>
+    <a className={styles.photo} href={link}>
+      <img src={image} alt="avatar" className={styles.photo__img}/>
+    </a>
+    <div className={styles.text}>{role}</div>
+    <List>
+      {dev.map(about => <ListItem key={about}><Done/>{about}</ListItem>)}
+    </List>
+  </div>);
