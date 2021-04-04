b165f8a6b5818da70fd8665836ca68bd79a0bba3
Max Rebedailo <akrosom21@gmail.com>
2021-04-04 17:07:46 +0300

    feat: add diagram for statistics in dictionary

--- src/Сomponents/Book/Dictionary/Studied/StudiedStatistics.tsx
+++ src/Сomponents/Book/Dictionary/Studied/StudiedStatistics.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { bookSections } from '../../../../helpers/constants';
+import { data } from '../../../../helpers/diagramData';
+import { Diagram } from './Diagram/Diagram';
 import styles from './StudiedStatistics.module.scss';
 
 export const StudiedStatistics = () => {
     const { sectionId } = useParams();
     const [level, image] = [bookSections[sectionId - 1].name, bookSections[sectionId - 1].image]
+    const diagramData = data
   return (
     <div className={styles.statistics}>
       <div className={styles.level}>
@@ -13,9 +16,13 @@ export const StudiedStatistics = () => {
         <img src={image} alt="planets" className={styles.category__icon} />
       </div>
       <h2 className={styles.title}>Статистика раздела:</h2>
-        <div className={styles.diagram}>O</div>
+        <div className={styles.diagram}>
+            <Diagram data={diagramData}/>
+        </div>
       <h2 className={styles.title}>Статистика страницы:</h2>
-        <div className={styles.diagram}>O</div>
+        <div className={styles.diagram}>
+        <Diagram data={diagramData}/>
+        </div>
     </div>
   );
 };
